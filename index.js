56e931ff1e3986c5b786566f3ea31a8c5b012641
Nex <nexinspiration@gmail.com>
2017-12-29 21:30:08 +0530

    basic functionality in place

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let filesInDir = fs.readdirSync('./');
 
 // Filter HTML Files
 let htmlFiles = filesInDir.filter((file) => {
-  return file.match(/[\w,\d]+\.html/g);
+  // fs.statSync(file).isFile()
+  return file.match(/[\w,\d]+\.html/g) ;
 });
 
 htmlFiles.forEach((file) => {
@@ -12,13 +13,11 @@ htmlFiles.forEach((file) => {
     if (err) {
       console.log(`can't read file: ${file}`)
     } else {
-
       // gets all matches in a file
       fs.readFile(fileToRead, { encoding: 'utf-8' }, (err, data) => {
         if (err) {
           console.log(`can't read file: ${file}`)
         } else {
-
           let matches = [];
           const regex = /{{inline [\', \"][^}]+[\', \"]}}/g
           let match;
@@ -28,19 +27,15 @@ htmlFiles.forEach((file) => {
               matches = [...matches, match[0]];
             }
           } while (match);
-
           matches.forEach((match) => {
             let fileNameToInline = match.match(/[\', \"]([^}]+)[\', \"]/g);
             fileNameToInline = fileNameToInline[0].replace(/[", ']/g, '');
-
-            let fileData = getFileData(fileNameToInline);
-            // console.log('fileNameToInline:', fileNameToInline);
-            // console.log('fileData:', fileData);
+            let fileData = getInlinesFileData(fileNameToInline);
             data = data.replace(match, fileData);
           });
 
-          console.log(matches);
-          console.log(data);
+
+          writeIntoFile(fileToRead, data);
         }
 
       });
@@ -48,7 +43,7 @@ htmlFiles.forEach((file) => {
   });
 });
 
-function getFileData(fileNameToInline) {
+function getInlinesFileData(fileNameToInline) {
   console.log(fileNameToInline);
   let data = fs.readFileSync(fileNameToInline, 'utf-8');
 
@@ -56,5 +51,11 @@ function getFileData(fileNameToInline) {
   return data;
 }
 
-//replace
-//var result = fileAsString.replace(/string to be replaced/g, 'replacement');
+function writeIntoFile(file, data) {
+  fs.writeFile(file, data, { encoding: 'utf-8' }, (err, data) => {
+    if (err) {
+      console.log(`can't write to file: ${file}`)
+    }
+    console.log(`The file ${file} has been saved!`);
+  });
+}
